cb0e1e8e440a51a3a93816ae09f05c7192a00e42
alhassan abdul-razak <alhassan.razak@amalitech.org>
2022-03-22 02:39:02 +0000

    button, error,link,and moviegrid files created

---
+++ src/components/styled-components/styled-components/Button.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+	padding: 2rem;
+	border: 0;
+	border-radius: var(--radius-300);
+	background: var(--red);
+	transition: 0.2s;
+
+	&:hover {
+		background: var(--light);
+		color: var(--dark);
+	}
+
+	&:disabled {
+		opacity: 0.5;
+	}
+`;
