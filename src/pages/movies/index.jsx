1a9c14f09a267eefc3fa42090bc487879614a4c8
alhassan abdul-razak <alhassan.razak@amalitech.org>
2022-03-22 06:22:58 +0000

    fetching data from movies

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -1,33 +1,33 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import MovieCard from '../../components/cards/MovieCard';
-// import { MovieGrid } from '../../components/styled-components/MovieGrid';
-// import { useMovies } from '../../hooks/useMovies/useMovies';
+import React from 'react';
+import styled from 'styled-components';
+import MovieCard from '../../components/cards/MovieCard';
+import { MovieGrid } from '../../components/styled-components/MovieGrid';
+import { useMovies } from '../../hooks/useMovies/useMovies';
 
-// function Movies() {
-// 	const { data: movies } = useMovies();
+function Movies() {
+	const { data: movies } = useMovies();
 
-// 	return (
-// 		<Container>
-// 			<h2>Movies</h2>
-// 			{movies ? (
-// 				<MovieGrid>
-// 					{movies
-// 						.filter(({ category }) => category === 'Movie')
-// 						.map((movie, i) => (
-// 							<MovieCard key={movie.title + i} movie={movie} />
-// 						))}
-// 				</MovieGrid>
-// 			) : (
-// 				<h2>Loading...</h2>
-// 			)}
-// 		</Container>
-// 	);
-// }
+	return (
+		<Container>
+			<h2>Movies</h2>
+			{movies ? (
+				<MovieGrid>
+					{movies
+						.filter(({ category }) => category === 'Movie')
+						.map((movie, i) => (
+							<MovieCard key={movie.title + i} movie={movie} />
+						))}
+				</MovieGrid>
+			) : (
+				<h2>Loading...</h2>
+			)}
+		</Container>
+	);
+}
 
-// export default Movies;
+export default Movies;
 
-// const Container = styled.div`
-// 	display: grid;
-// 	gap: 2rem;
-// `;
+const Container = styled.div`
+	display: grid;
+	gap: 2rem;
+`;
