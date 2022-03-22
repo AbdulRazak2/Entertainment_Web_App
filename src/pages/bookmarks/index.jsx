5687150b10960c24a41eb58f5f9726421d2af9f0
alhassan abdul-razak <alhassan.razak@amalitech.org>
2022-03-22 06:16:20 +0000

    add tv-shows file in page components

---
+++ src/pages/bookmarks/index.jsx
@@ -0,0 +1,34 @@
+// import React from 'react';
+// import styled from 'styled-components';
+// import MovieCard from '../../components/cards/MovieCard';
+// import { MovieGrid } from '../../components/styled-components/MovieGrid';
+// import { useBookmarks } from '../../hooks/useBookmark/useBookmarks';
+// import { useMovies } from '../../hooks/useMovies/useMovies';
+
+// function Bookmarks() {
+// 	const { data: bookmarks } = useBookmarks();
+// 	const { data: movies } = useMovies();
+
+// 	return (
+// 		<Container>
+// 			<h2>Bookmarks</h2>
+// 			{movies && bookmarks ? (
+// 				<MovieGrid>
+// 					{movies
+// 						.filter(({ id }) => bookmarks[id])
+// 						.map((movie) => (
+// 							<MovieCard key={movie.id} movie={movie} />
+// 						))}
+// 				</MovieGrid>
+// 			) : (
+// 				<h2>Loading...</h2>
+// 			)}
+// 		</Container>
+// 	);
+// }
+
+// export default Bookmarks;
+// const Container = styled.div`
+// 	display: grid;
+// 	gap: 2rem;
+// `;
---
+++ src/pages/search-results/index.jsx
@@ -0,0 +1,9 @@
+// import { useMovies } from '../../hooks/useMovies/useMovies';
+// import Render from './Render';
+
+// function SearchResults() {
+// 	const { data: movies } = useMovies();
+// 	return movies ? <Render movies={movies} /> : <h2>Searching...</h2>;
+// }
+
+// export default SearchResults;
