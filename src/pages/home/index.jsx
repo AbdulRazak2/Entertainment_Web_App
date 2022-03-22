08d8970addaa17c53c53be8fea4cc0943d66f259
alhassan abdul-razak <alhassan.razak@amalitech.org>
2022-03-22 07:30:39 +0000

    movies h2 and loading h2 added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,53 +1,53 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import MovieCard from '../../components/cards/MovieCard';
-// import TrendingCard from '../../components/cards/TrendingCard';
-// import { MovieGrid } from '../../components/styled-components/MovieGrid';
-// import { useMovies } from '../../hooks/useMovies/useMovies';
+import React from 'react';
+import styled from 'styled-components';
+import MovieCard from '../../components/cards/MovieCard';
+import TrendingCard from '../../components/cards/TrendingCard';
+import { MovieGrid } from '../../components/styled-components/MovieGrid';
+import { useMovies } from '../../hooks/useMovies/useMovies';
 
-// function Home() {
-// 	const { data: movies } = useMovies();
+function Home() {
+	const { data: movies } = useMovies();
 
-// 	return movies ? (
-// 		<Container>
-// 			<h2>Trending</h2>
-// 			<TrendingGrid>
-// 				{movies
-// 					.filter(({ isTrending }) => isTrending)
-// 					.map((movie) => (
-// 						<TrendingCard key={movie.id} movie={movie} />
-// 					))}
-// 			</TrendingGrid>
+	return movies ? (
+		<Container>
+			<h2>Trending</h2>
+			<TrendingGrid>
+				{movies
+					.filter(({ isTrending }) => isTrending)
+					.map((movie) => (
+						<TrendingCard key={movie.id} movie={movie} />
+					))}
+			</TrendingGrid>
 
-// 			<h2>Recommended for you</h2>
-// 			<MovieGrid>
-// 				{movies
-// 					.filter(({ category }) => category === 'Movie')
-// 					.map((movie) => (
-// 						<MovieCard key={movie.id} movie={movie} />
-// 					))}
-// 			</MovieGrid>
-// 		</Container>
-// 	) : (
-// 		<h2>Loading...</h2>
-// 	);
-// }
+			<h2>Recommended for you</h2>
+			<MovieGrid>
+				{movies
+					.filter(({ category }) => category === 'Movie')
+					.map((movie) => (
+						<MovieCard key={movie.id} movie={movie} />
+					))}
+			</MovieGrid>
+		</Container>
+	) : (
+		<h2>Loading...</h2>
+	);
+}
 
-// export default Home;
+export default Home;
 
-// const Container = styled.div`
-// 	display: grid;
-// 	gap: 2rem;
-// `;
+const Container = styled.div`
+	display: grid;
+	gap: 2rem;
+`;
 
-// const TrendingGrid = styled.div`
-// 	width: 100%;
-// 	overflow-x: auto;
-// 	display: flex;
-// 	gap: 2rem;
-// 	margin-bottom: 1rem;
+const TrendingGrid = styled.div`
+	width: 100%;
+	overflow-x: auto;
+	display: flex;
+	gap: 2rem;
+	margin-bottom: 1rem;
 
-// 	&::-webkit-scrollbar {
-// 		display: none;
-// 	}
-// `;
+	&::-webkit-scrollbar {
+		display: none;
+	}
+`;
