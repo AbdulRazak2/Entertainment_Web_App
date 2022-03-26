c34ea60d096632a18f8afec65f9a68c8252db578
alhassan abdul-razak <alhassan.razak@amalitech.org>
2022-03-26 23:15:35 +0000

    add commentst to firebase file

--- src/components/cards/SharedStyles.js
+++ src/components/cards/SharedStyles.js
@@ -1,63 +1,63 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 //Bookmark styled using styled-components
 
 export const BookmarkBtn = styled.button`
-	position: absolute;
-	top: 2rem;
-	right: 2rem;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	border-radius: 50%;
-	background: var(--dark-transparent);
-	border: 0;
-	width: 3.5rem;
-	height: 3.5rem;
-	transition: all.2s;
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background: var(--dark-transparent);
+  border: 0;
+  width: 3.5rem;
+  height: 3.5rem;
+  transition: all.2s;
 
-	&:hover {
-		transform: scale(1.1);
-	}
+  &:hover {
+    transform: scale(1.1);
+  }
 
-	&:active {
-		transform: scale(0.95);
-	}
+  &:active {
+    transform: scale(0.95);
+  }
 `;
 
 //BookmarkIcon and image styled
 export const BookmarkIcon = styled.img`
-	opacity: ${(p) => (p.visible ? '1' : '0')};
-	position: ${(p) => (p.visible ? 'unset' : 'absolute')};
+  opacity: ${(p) => (p.visible ? "1" : "0")};
+  position: ${(p) => (p.visible ? "unset" : "absolute")};
 `;
 
 export const Info = styled.div`
-	opacity: 0.5;
-	display: flex;
-	align-items: center;
-	gap: 0.8rem;
+  opacity: 0.5;
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
 `;
 
 export const Dot = styled.span`
-	width: 0.5rem;
-	height: 0.5rem;
-	border-radius: 50%;
-	background: var(--light);
+  width: 0.5rem;
+  height: 0.5rem;
+  border-radius: 50%;
+  background: var(--light);
 `;
 
-//Display category styled 
+//Display category styled
 
 export const Category = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 0.5rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 
-	svg {
-		transform: scale(0.8);
-	}
+  svg {
+    transform: scale(0.8);
+  }
 `;
 
 export const CategoryIcon = styled.div`
-	fill: var(--light);
-	height: auto.1rem;
+  fill: var(--light);
+  height: auto.1rem;
 `;
